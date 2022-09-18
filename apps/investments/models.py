9ed8a77e2fcea092329af95f6398782393effe8f
한송희 <98_han@naver.com>
2022-09-18 10:03:58 +0900

    Fix : investment 테이블 외래키 필드 이름 수정
    - 외래키인 account_number_id를 서비스 흐름에 맞게 account_id로 바꾸도록 수정

--- apps/investments/models.py
+++ apps/investments/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Investment(models.Model):
     # 증권 계좌를 삭제한다는 것은 더이상 투자를 하지 않는다는 것으로 판단하여 같이 삭제되도록 설정
-    account_number = models.ForeignKey("accounts.Account", on_delete=models.CASCADE,
+    account = models.ForeignKey("accounts.Account", on_delete=models.CASCADE, db_column='account_id',
                                        related_name="investment_account_number")
     # 증권이 사라진다고 해도 투자내역은 남아있어야 하기 때문에 참조 무결성을 해치더라도 깂을 그대로 저장하도록 설정
     investment_stock = models.ForeignKey("stocks.Stock", on_delete=models.DO_NOTHING, db_column='investment_stock_isin',
