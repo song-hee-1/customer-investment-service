17f9a12b09d725778d006f699b188d7047e9e413
한송희 <98_han@naver.com>
2022-09-20 18:26:56 +0900

    Refactor : 데이터 파일 불러오는 위치 수정
    - 편리성을 위하여 데이터 파일을 불러오는 위치를 맨 앞으로 수정

--- apps/stocks/cron.py
+++ apps/stocks/cron.py
@@ -13,8 +13,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 @transaction.atomic()
 def crontab_stock_app_job():
-    filename = os.path.join(BASE_DIR, 'stocks/data/stock_table.xlsx')
-    stock_info_set = pd.read_excel(filename)
+
+    asset_group_data = os.path.join(BASE_DIR, 'stocks/data/asset_group_table.xlsx')
+    asset_group_info_set = pd.read_excel(asset_group_data)
+
+    for i, row in asset_group_info_set.iterrows():
+        AssetGroup.objects.update_or_create(
+            id=row['id'],
+            defaults={
+                'name': row['name'],
+            }
+        )
+
+    print(f"{datetime.now()} : Successfully update asset group table")
+
+    stock_data = os.path.join(BASE_DIR, 'stocks/data/stock_table.xlsx')
+    stock_info_set = pd.read_excel(stock_data)
 
     for i, row in stock_info_set.iterrows():
         Stock.objects.update_or_create(
@@ -27,16 +41,3 @@ def crontab_stock_app_job():
         )
 
     print(f"{datetime.now()} : Successfully update stock table")
-
-    filename = os.path.join(BASE_DIR, 'stocks/data/asset_group_table.xlsx')
-    asset_group_info_set = pd.read_excel(filename)
-
-    for i, row in asset_group_info_set.iterrows():
-        AssetGroup.objects.update_or_create(
-            id=row['id'],
-            defaults={
-                'name': row['name'],
-            }
-        )
-
-    print(f"{datetime.now()} : Successfully update asset group table")
