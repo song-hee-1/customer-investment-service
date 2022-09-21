073cc60ef3d95b6a240c032f931ae9ceafccc2ef
한송희 <98_han@naver.com>
2022-09-21 19:31:19 +0900

    Feat : Azure 배포를 위해 sqlite에서 postgresql로 데이터 베이스 변경

--- config/settings.py
+++ config/settings.py
@@ -106,8 +106,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fint',
+        'USER': get_secret('USER'),
+        'PASSWORD': get_secret('PASSWORD'),
+        'HOST': get_secret('HOST'),
+        'PORT': '5432',
     }
 }
 
