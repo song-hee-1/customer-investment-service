631a10d2664f7be6f6d713d4dd1173c45b1a1cdf
한송희 <98_han@naver.com>
2022-09-21 14:51:55 +0900

    Refactor : 반복되는 계산식을 변수로 저장하여 이용하도록 수정
    - total_aset 계산식을 반복해서 이용하므로 그 과정을 market_value_formula로 만들어 정리

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -34,29 +34,21 @@ class InvestDetailSerializer(serializers.ModelSerializer):
         depth = 1
         exclude = ['user', 'create_time', 'investment']
 
-    # 고민 : total_asset 계산식이 여러번 사용되는데, 상속하는 등 다시 이용할 수 없는 방법은 없는 것인가?
+    # total_asset 계산식이 반복돼서 refactor 하고 싶었지만, SerializerMethodField는 상속이 불가능
+    # 계산식을 market_value_formula로 만들어 이용
     def get_account_total_asset(self, obj):
         total_asset = Account.objects.filter(id=obj.id).values('id').annotate(
-            account_total_asset=Sum(
-                F('investment_account_number__quantity') *
-                F('investment_account_number__investment_stock__current_price')
-            ))[0]['account_total_asset']
+            account_total_asset=market_value_formula)[0]['account_total_asset']
         return total_asset
 
     def get_total_profit(self, obj):
         total_asset = Account.objects.filter(id=obj.id).values('id').annotate(
-            account_total_asset=Sum(
-                F('investment_account_number__quantity') *
-                F('investment_account_number__investment_stock__current_price')
-            ))[0]['account_total_asset']
+            account_total_asset=market_value_formula)[0]['account_total_asset']
         return total_asset - obj.account_total_investment_principal
 
     def get_rate_of_return(self, obj):
         total_asset = Account.objects.filter(id=obj.id).values('id').annotate(
-            account_total_asset=Sum(
-                F('investment_account_number__quantity') *
-                F('investment_account_number__investment_stock__current_price')
-            ))[0]['account_total_asset']
+            account_total_asset=market_value_formula)[0]['account_total_asset']
         total_profit = total_asset - obj.account_total_investment_principal
         return round((total_profit / obj.account_total_investment_principal) * 100, 2)
 
@@ -70,3 +62,7 @@ class InvestSerializer(InvestDetailSerializer):
         model = Account
         depth = 1
         exclude = ['user', 'create_time', 'investment', 'account_total_investment_principal']
+
+
+market_value_formula = Sum(
+    F('investment_account_number__quantity') * F('investment_account_number__investment_stock__current_price'))
